3373a2d5550f9bf7e00498c5534c75f566418f17
Trim07 <dxotabiladeiro1@gmail.com>
2020-01-02 07:42:36 -0300

    Project started

---
+++ src/main/java/com/hcg/kuririn/KuririnApplication.java
@@ -0,0 +1,13 @@
+package com.hcg.kuririn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuririnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuririnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcg/kuririn/KuririnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcg.kuririn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuririnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
